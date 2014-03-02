0125cd87d7c5c7c1df14e5ecc1bd9d3cfe8beef3
W1lla van den Munckhof <w1llaopgezwolle@gmail.com>
2014-03-02 15:50:16 +0100

    update minor bug

--- Shootmania/Elite/Elite.php
+++ Shootmania/Elite/Elite.php
@@ -1458,7 +1458,7 @@ PRIMARY KEY (`id`)
         	$dataDir = $this->connection->gameDataDirectory();
         	$dataDir = str_replace('\\', '/', $dataDir);
         	$file = $this->connection->getServername();
-        	$name = $this=>filterName($file);
+        	$name = $this->filterName($file);
         	$challengeFile = $dataDir . "Replays/" . $name;
 													var_dump($challengeFile);
 // set server back to old value.
@@ -1509,38 +1509,37 @@ PRIMARY KEY (`id`)
     }
 
 /**
-üü üü üü* filterName()
-üü üü üü* Function used to filter the tracks filename.
-üü üü üü*
-üü üü üü* @param mixed $text
-üü üü üü* @return string $output
-üü üü üü*/
-üü üü function filterName($text) {
-üü üü üü üü $str = trim(utf8_decode($text));
-üü üü üü üü $output = "";
-üü üü üü üü for ($i = 0; $i < strlen($str); $i++) {
-üü üü üü üü üü üü $c = ord($str[$i]);
-üü üü üü üü üü üü if ($c == 32) {
-üü üü üü üü üü üü üü üü $output .= "_";
-üü üü üü üü üü üü üü üü continue;
-üü üü üü üü üü üü } // space
-üü üü üü üü üü üü if ($c >= 48 && $c <= 57) {
-üü üü üü üü üü üü üü üü $output .= chr($c);
-üü üü üü üü üü üü üü üü continue;
-üü üü üü üü üü üü }// 0-9
-üü üü üü üü üü üü if ($c >= 65 && $c <= 90) {
-üü üü üü üü üü üü üü üü $output .= chr($c);
-üü üü üü üü üü üü üü üü continue;
-üü üü üü üü üü üü }// A-Z
-üü üü üü üü üü üü if ($c >= 97 && $c <= 122) {
-üü üü üü üü üü üü üü üü $output .= chr($c);
-üü üü üü üü üü üü üü üü continue;
-üü üü üü üü üü üü }// a-z
-üü üü üü üü üü üü $output .= "_";
-üü üü üü üü }
-üü üü üü üü return utf8_encode($output);
-üü üü }
-
+* filterName()
+* Function used to filter the tracks filename.
+*
+* @param mixed $text
+* @return string $output
+*/
+ function filterName($text) {
+ $str = trim(utf8_decode($text));
+ $output = "";
+ for ($i = 0; $i < strlen($str); $i++) {
+ $c = ord($str[$i]);
+if ($c == 32) {
+ $output .= "_";
+ continue;
+ } // space
+ if ($c >= 48 && $c <= 57) {
+ $output .= chr($c);
+ continue;
+ }// 0-9
+if ($c >= 65 && $c <= 90) {
+ $output .= chr($c);
+ continue;
+ }// A-Z
+ if ($c >= 97 && $c <= 122) {
+ $output .= chr($c);
+ continue;
+ }// a-z
+ $output .= "_";
+  }
+ return utf8_encode($output);
+		}
 }
 
 ?>
