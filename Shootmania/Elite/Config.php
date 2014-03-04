5329626b6a07de9b84c75afc6ffae9823fc5891b
W1lla van den Munckhof <w1llaopgezwolle@gmail.com>
2014-03-04 18:14:30 +0100

    added config.php

---
+++ Shootmania/Elite/Config.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @copyright   Copyright (c) 2009-2012 NADEO (http://www.nadeo.com)
+ * @license     http://www.gnu.org/licenses/lgpl.html LGPL License 3
+ * @version     $Revision: $:
+ * @author      $Author: $:
+ * @date        $Date: $:
+ */
+
+namespace ManiaLivePlugins\Shootmania\Elite;
+
+class Config extends \ManiaLib\Utils\Singleton
+{
+
+	/**
+	 * @var int
+	 */
+	public $competition_id = 0;
+
+?>
