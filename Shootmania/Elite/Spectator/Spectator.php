b0ee1236846bc8dcbb87b59e37a4c15c97632b60
W1lla van den Munckhof <w1llaopgezwolle@gmail.com>
2014-04-08 22:21:43 +0200

    version update

--- Shootmania/Elite/Spectator/Spectator.php
+++ Shootmania/Elite/Spectator/Spectator.php
@@ -74,7 +74,7 @@ class Spectator extends \ManiaLive\PluginHandler\Plugin {
     private $playerIDs = array();
  
     function onInit() {
-        $this->setVersion('1.0.5d');
+        $this->setVersion('1.0.5e');
 		
         $this->logger = new Log($this->storage->serverLogin);
 	}
